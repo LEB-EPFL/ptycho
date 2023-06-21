240281f1cbfcb06d17729a188f2cd1f8d9edb041
Christian Zimmerli <christian.zimmerli@epfl.ch>
2023-06-21 15:01:59 +0200

    created loops over iteration of images

--- python_src/leb/freeze/fp.py
+++ python_src/leb/freeze/fp.py
@@ -1,2 +1,14 @@
-def fp_recover():
-    pass
+from enum import Enum
+import numpy as np
+
+class Method(Enum):
+    rPIE = "rPIE"
+    GD = "GD"
+
+def fp_recover(images: np.array, num_iterations: int = 10, method: Method = Method.rPIE):
+    num_images = images.shape[0]
+
+    for i in range(num_iterations):
+        for j in range(num_images):
+        
+            pass
--- tests/leb-freeze/test_fp.py
+++ tests/leb-freeze/test_fp.py
@@ -1,2 +1,9 @@
+from leb.freeze.fp import fp_recover, Method
+import numpy as np
+
 def test_fp_recover():
+    imgs = np.random.random((10,32,32))
+
+    method = Method.rPIE
+    fp_recover(imgs, method=method)
     raise NotImplementedError
