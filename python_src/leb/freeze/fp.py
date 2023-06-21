f1a581dddcecc9544b697eaa7e3984ae31eec3ed
Christian Zimmerli <christian.zimmerli@epfl.ch>
2023-06-21 15:17:42 +0200

    rescaling initial_object and FFT

--- python_src/leb/freeze/fp.py
+++ python_src/leb/freeze/fp.py
@@ -1,12 +1,25 @@
 from enum import Enum
 import numpy as np
+from numpy.fft import fft2, fftshift
+from skimage.transform import rescale
 
 class Method(Enum):
     rPIE = "rPIE"
     GD = "GD"
 
-def fp_recover(images: np.array, num_iterations: int = 10, method: Method = Method.rPIE):
+def fp_recover(
+    images: np.array, 
+    num_iterations: int = 10, 
+    method: Method = Method.rPIE,
+    scaling_factor: int = 4
+):
     num_images = images.shape[0]
+
+    initial_object = np.mean(images, axis=0)
+    initial_object = rescale(initial_object, scaling_factor)
+    f_inital_object = fftshift(fft2(initial_object))
+
+
 
     for i in range(num_iterations):
         for j in range(num_images):
