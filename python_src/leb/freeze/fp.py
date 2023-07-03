Fix scaling factors for FFTs

python_src/leb/freeze/fp.py
@@ -66,9 +66,10 @@ def fp_recover(
 
     # Though we are upsampling the target, the pupil sampling rate dk remains unchanged because
     # the upsampling is performed to add pixels to the FFT, not to improve k-space resolution!
+    dx = 2 * np.pi / pupil.k_S  # Spacing between object pixels
     initial_object = np.mean(dataset.images, axis=0)
     target = rescale(initial_object, upsampling_factor)
-    target_fft = fftshift(fft2(target))
+    target_fft = dx * dx * fftshift(fft2(target))
     target_pupil = deepcopy(pupil)
 
     original_size_px = dataset.images.shape[1]
@@ -98,15 +99,14 @@ def fp_recover(
             low_res_img_fft = current_slice_fft * target_pupil.p
 
             # Compute the low resolution image from the current slice
-            # TODO Verify that multiplication by dk **2 is correct.
-            low_res_img = target_pupil.dk**2 * ifft2(ifftshift(low_res_img_fft))
+            low_res_img = ifft2(ifftshift(low_res_img_fft)) / dx / dx
 
             # Replace the amplitude of the low res. image with the measured amplitude.
             # Leave the phase unchanged.
             low_res_img = np.abs(image) * np.exp(1j * np.angle(low_res_img))
 
             # Update the target_fft with the new slice data using the rPIE algorithm
-            next_low_res_img_fft = fftshift(fft2(low_res_img))
+            next_low_res_img_fft = dx * dx * fftshift(fft2(low_res_img))
             current_slice_fft += (
                 np.conj(target_pupil.p)
                 / (
@@ -133,8 +133,7 @@ def fp_recover(
                     continue
 
     # Compute the final complex object
-    # TODO Verify that multiplication by dk **2 is correct.
-    return target_pupil.dk**2 * ifft2(ifftshift(target_fft)), target_pupil
+    return ifft2(ifftshift(target_fft)) / dx / dx, target_pupil
 
 
 def slice_fft(

python_src/leb/freeze/simulation.py
@@ -90,7 +90,8 @@ def generate_simulated_images(
     gt: NDArray[np.complex128], calibration: Calibration, pupil: Pupil
 ) -> NDArray[np.float64]:
     """Generates simulated images from a ground truth object."""
-    gt_fft = fftshift(fft2(gt))
+    dx = 2 * np.pi / pupil.k_S
+    gt_fft = dx * dx * fftshift(fft2(gt))
 
     dataset_size_px = pupil.p.shape[0]
     num_images = len(calibration)
@@ -108,10 +109,10 @@ def generate_simulated_images(
 
         # Copy the slice, then low pass filter it with the pupil
         current_slice_fft = current_slice_fft.copy()
-        current_slice_fft *= pupil.p  # TODO Add abberrations here
+        current_slice_fft *= pupil.p
 
         # Inverse FFT to obtain the image
-        current_image = pupil.dk**2 * np.abs(ifft2(ifftshift(current_slice_fft)))
+        current_image = np.abs(ifft2(ifftshift(current_slice_fft)) / dx / dx)
 
         images[image_num] = current_image
 